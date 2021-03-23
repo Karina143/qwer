c365536c87bd8703f60629597368e1ca22c30b21
Karina143 <81224928+Karina143@users.noreply.github.com>
2021-03-23 12:59:48 +0300

    Add files via upload

---
+++ norm_cube.py
@@ -0,0 +1,8 @@
+from graph import*
+penSize(1)
+penColor("black")
+brushColor("yellow")
+rectangle(20,30,60,70)
+brushColor("orange")
+polygon([(20,30),(30,20),(70,20),(60,30),(20,30)])
+polygon([(60,30),(70,20),(70,60),(60,70),(60,30)])
