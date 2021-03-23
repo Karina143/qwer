5e54aaa6116cdbb2347e0a10eae1d744891afc6b
Karina143 <81224928+Karina143@users.noreply.github.com>
2021-03-23 13:14:10 +0300

    Add files via upload

---
+++ cubetwo.py
@@ -0,0 +1,8 @@
+from graph import*
+penSize(1)
+penColor("black")
+brushColor("green")
+polygon([(20,30),(20,60),(50,70),(50,40),(20,30)])
+polygon([(50,40),(50,70),(80,60),(80,30),(50,40)])
+brushColor("blue")
+polygon([(20,30),(50,40),(80,30),(50,20),(20,30)])
